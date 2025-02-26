b64935ef7e5e829cc0c3e34b0d24c8cccea55732
User-01 <mateusmanoelfr@gmail.com>
2025-02-25 21:30:49 -0300

    first

---
+++ src/test/java/com/example/granaguru/GranaguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.granaguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranaguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
